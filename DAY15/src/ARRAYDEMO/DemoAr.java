024e5f971b0945262dbb46e9cd122b3bf1e3da54
Raj <Raj@DESKTOP-20QU2N8>
2022-03-27 09:32:40 +0000

    first commit

---
+++ DAY15/src/ARRAYDEMO/DemoAr.java
@@ -0,0 +1,21 @@
+package ARRAYDEMO;
+
+public class DemoAr {
+	public static void main(String[]args) {
+		int Arr1[][]=new[3][3];
+		int arr[][]= {{2,7,9},{3,6,1},{7,4,2}};
+		arr1[0]=new int[3];
+		arr1[1]=new int[3];
+		arr1[2]=new int[3];
+	for(inti=0;i<3;i++);
+	{
+		for(int j=0;j<3;j++)
+		{
+			System.out.println(arr[i][j]);
+		}
+		System.out.println();
+		}
+	}
+	}
+
+
---
+++ DAY15/src/ARRAYDEMO/DemoArr.java
@@ -0,0 +1,24 @@
+package ARRAYDEMO;
+
+public class DemoArr {
+public static void main(String[] args) {
+		int a[]=new int[5];
+		a[0]=34;
+		a[1]=12;
+		a[2]=3;
+		a[3]=78;
+		a[4]=2;
+
+	for(int i=0;i<a.length;i++);
+	{
+
+		
+		int i = 0;
+		
+		System.out.println(a[i]);
+		
+	}
+
+	}
+
+}
