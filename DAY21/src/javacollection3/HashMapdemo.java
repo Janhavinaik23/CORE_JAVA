6fee8dd063a090957e092ff9cb0141cf716bf5e8
Raj <Raj@DESKTOP-20QU2N8>
2022-04-24 10:39:02 +0000

    first commit

---
+++ DAY21/src/javacollection3/HashMapdemo.java
@@ -0,0 +1,18 @@
+package javacollection3;
+
+import java.util.HashMap;
+
+public class HashMapdemo {
+
+	public static void main(String[] args) {
+		HashMap<Integer,String> hmap=new HashMap<Integer,String>();
+		hmap.put(1, "janhavi");
+		hmap.put(2, "shital");
+		System.out.println(hmap);
+
+	}
+}
+
+
+
+
